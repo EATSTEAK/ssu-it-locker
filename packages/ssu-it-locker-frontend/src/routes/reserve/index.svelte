9e75d72e64478b1b7403d277026a72d0103c4477
EATSTEAK <eatsteak95@gmail.com>
2022-01-02 09:03:26 +0900

    feat: create reserve page

---
+++ packages/ssu-it-locker-frontend/src/routes/reserve/index.svelte
@@ -0,0 +1,27 @@
+<script lang='ts'>
+	import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'sveltestrap';
+
+	let isOpen;
+</script>
+
+<main>
+	<Navbar color='dark' class='shadow-lg' dark expand='xs'>
+		<NavbarBrand>사물함 예약 시스템</NavbarBrand>
+		<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+		<Collapse {isOpen} navbar expand='xs'>
+			<Nav class='me-auto' navbar>
+				<NavItem>
+					<NavLink href='#' active>예약하기</NavLink>
+				</NavItem>
+				<NavItem>
+					<NavLink href='/logout'>로그아웃</NavLink>
+				</NavItem>
+			</Nav>
+		</Collapse>
+	</Navbar>
+	<section class='container'>
+		<section class='my-3 mx-1 p-3 shadow-lg '>
+
+		</section>
+	</section>
+</main>
